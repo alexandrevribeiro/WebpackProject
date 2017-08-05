e9b11326bfc8807188c890015469a8590d3c0b7d
alexandrevribeiro <vribeiro.alexandre@gmail.com>
2017-08-05 18:39:20 -0300

    Adding CSS loaders to the Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,15 @@ module.exports = {
         use: 'babel-loader',
         test: /\.js$/,
         exclude: /node_modules/
+      },
+      {
+        // Remembering... The loaders are executed from the right to the left.
+        // 1º) "css-loader": Allows Webpack to understand  and read the contents of CSS files
+        // that are imported into the project structure.
+        // 2º) "style-loader": Takes all the CSS modules and sticks them into a style tag inside
+        // the index.html document.
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
       }
     ]
   }
